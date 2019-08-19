8b9fda5a4c432dffc915455fc19d298c758324e9
nielsdelestinne <nielsdelestinne@gmail.com>
2019-08-19 13:43:06 +0200

    Test added + part of build

---
+++ src/test/java/com/switchfully/dicer/DicerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.switchfully.dicer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DicerApplicationTest {
+
+    @Test
+    void justADummyTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
